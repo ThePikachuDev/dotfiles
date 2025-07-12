ac9899e4322fe6e56ee75909578d4ff025ed82a5
ThePikachuDev <meharwansinghlehri@gmail.com>
2025-07-12 19:03:14 +0530

    updated kitty config and made a script to download my plugins

---
+++ zsh-setup.sh
@@ -0,0 +1,8 @@
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install zsh-autosuggestions and zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
